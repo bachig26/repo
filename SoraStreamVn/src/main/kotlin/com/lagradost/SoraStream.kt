8ee1b567bbd8f91bbadef89263873a2d1f4fe843
tuan041 <30403510+tuan041@users.noreply.github.com>
2023-01-28 12:56:32 +0700

    Update SoraStream.kt

--- SoraStreamVn/src/main/kotlin/com/lagradost/SoraStream.kt
+++ SoraStreamVn/src/main/kotlin/com/lagradost/SoraStream.kt
@@ -662,6 +662,8 @@ open class SoraStream : TmdbProvider() {
         @JsonProperty("release_date") val releaseDate: String? = null,
         @JsonProperty("first_air_date") val firstAirDate: String? = null,
         @JsonProperty("overview") val overview: String? = null,
+        @JsonProperty("episode_run_time") val episode_run_time: String? = null,
+        @JsonProperty("run_time") val run_time: String? = null,
         @JsonProperty("vote_average") val vote_average: Any? = null,
         @JsonProperty("original_language") val original_language: String? = null,
         @JsonProperty("status") val status: String? = null,
