6d3e74e26f313594f88637e500ba4f82c38ffe89
tuan041 <30403510+tuan041@users.noreply.github.com>
2023-04-07 20:35:21 +0700

    Update SoraStream.kt

--- SoraStreamVn/src/main/kotlin/com/lagradost/SoraStream.kt
+++ SoraStreamVn/src/main/kotlin/com/lagradost/SoraStream.kt
@@ -319,7 +319,7 @@ open class SoraStream : TmdbProvider() {
                 this.backgroundPosterUrl = bgPoster
                 this.year = year
                 this.plot = vi.overview
-                this.duration = res.runtime?.average()?.toInt()
+                this.duration = eps.runtime?.average()?.toInt()
                 this.tags = if (isAnime) keywords else genres
                 this.rating = rating
                 this.showStatus = getStatus(res.status)
