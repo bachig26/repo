32fec1c74250b928038f11fd51e6735544479e1b
tuan041 <30403510+tuan041@users.noreply.github.com>
2023-07-11 23:15:10 +0700

    Update TocanimeProvider.kt

--- Tocanime/src/main/kotlin/com/hexated/TocanimeProvider.kt
+++ Tocanime/src/main/kotlin/com/hexated/TocanimeProvider.kt
@@ -93,14 +93,18 @@ class TocanimeProvider : MainAPI() {
                 ?.substringBefore("\"")
         val description = document.select("div.box-content > p").text()
         val type =
-            if (document.select("div.me-list.scroller a").text().contains("Full Vietsub")) TvType.AnimeMovie
+            if (document.select("a.me-item.active.last").text().contains("Full Vietsub")) TvType.AnimeMovie
             else TvType.Anime
         val status = getStatus(
                 document.select("dl.movie-des dd.text-danger").text()
                     .toString()
             )
         val year = document.select("dl.movie-des").text()?.substringAfter("Ngày công chiếu :")
-                ?.substringBefore("Số tập :")?.trim()?.split("/")?.last()?.toIntOrNull()
+                ?.substringBefore("Số tập :")
+                ?.trim()
+                ?.split("/")
+                ?.last()
+                ?.toIntOrNull()
         val tags = document.select("ul.color-list li").map { it.select("a").text().removeSuffix(",").trim() }
         val episodes = document.select("div.me-list.scroller a").mapNotNull {
             Episode(fixUrl(it.attr("href")), it.text())
